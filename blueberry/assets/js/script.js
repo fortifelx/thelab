e6fa59d1aa1171d9811d4030c085754378c7d536
Fortifelx <fortifelx@gmail.com>
2019-01-21 13:02:16 +0200

    add resize option

--- blueberry/assets/js/script.js
+++ blueberry/assets/js/script.js
@@ -1,4 +1,8 @@
 (function(){
     var height = $('.contacts_data').css('height');
     $('.contacts_data_flex').css('height', height);
+    $( window ).resize(function(){
+        var height = $('.contacts_data').css('height');
+        $('.contacts_data_flex').css('height', height);
+    });
 })();
